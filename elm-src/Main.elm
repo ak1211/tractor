7bc15eaf12a9afeed952e2e13b7c28b1daf7c776
Akihiro Yamamoto <ak1211@mail.ak1211.com>
2018-05-13 22:31:51 +0900

    add: webclient

--- elm-src/Main.elm
+++ elm-src/Main.elm
@@ -1,3 +1,33 @@
+{-
+   This file is part of Tractor.
+
+   Tractor is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Tractor is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Tractor.  If not, see <http://www.gnu.org/licenses/>.
+-}
+{- |
+   Module      :  Main
+   Description :  This file is web front main module of Application "Tractor"
+   Copyright   :  (c) 2016 Akihiro Yamamoto
+   License     :  AGPLv3
+
+   Maintainer  :  https://github.com/ak1211
+   Stability   :  unstable
+   Portability :  POSIX
+
+   アプリケーション「Tractor」のフロント側メインモジュールです。
+-}
+
+
 module Main exposing (..)
 
 import Http
